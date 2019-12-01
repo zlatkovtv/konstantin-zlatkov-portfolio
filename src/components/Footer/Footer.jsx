4fb02ca7504e0a71a6f46e1ced178d0c41f6640a
Garrett <zlatkovtv@gmail.com>
2019-11-30 17:10:18 -0800

    Minor fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = (props) => {
     return (
         <footer id="footer">
             <a href="https://github.com/zlatkovtv/konstantin-zlatkov-portfolio" target="_blank" rel="noopener noreferrer">
-                <div className="footer-text">Designed and built with ❤️</div>
+                <div className="footer-text">Designed and built with React</div>
                 <div className="footer-text">by Konstantin Zlatkov</div>
             </a>
         </footer>
